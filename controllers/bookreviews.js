271c83b0093fe8157fac2cbf2fd25c77a03c59c2
Charles Adams 19 <adamscra19@gmail.com>
2025-04-19 14:08:01 +0100

    git commit -m edit and update routes

--- controllers/bookreviews.js
+++ controllers/bookreviews.js
@@ -21,7 +21,67 @@ return res.render('bookreviews/new.ejs')
     }
 })
 
+// Edit - Displays the form that allows us to submit an UPDATE request
+router.get('/bookreviews/:bookreviewId/edit', async (req, res, next) => {
+    try {
+        if (!mongoose.isValidObjectId(req.params.bookreviewId)) {
+            return next()
+        }
+        const bookreview = await BookReview.findById(req.params.bookreviewId)
 
+        if (!bookreview) return next()
+
+        return res.render('bookreviews/edit.ejs', {
+            bookreview: bookreview
+
+        })
+    } catch (error) {
+        return next(error)
+    }
+})
+
+// Show - displays a single article
+router.get('/bookreviews/:bookreviewId', async (req, res,next) =>{
+    try {
+    if (!mongoose.isValidObjectId(req.params.bookreviewId))  {
+        return next ()
+    } 
+    const bookreview = await BookReview.findById(req.params.bookreviewId).populate('reviewer')
+    if (!bookreview) return next()
+
+        return res.render('bookreviews/show.ejs', {
+        bookreview: bookreview
+        })
+    }catch (error) {
+
+    }
+})
+
+// Update - allows us to update an existing article
+
+router.put ('/bookreviews/:bookreviewId', async (req, res) =>{
+    try { 
+        const bookreviewId = req.params.bookreviewId 
+
+        if (!mongoose.isValidObjectId(bookreviewId)) {
+            return next ();
+        }
+        const bookreview = await BookReview.findById(bookreviewId)
+        if (!updatedBookReview ) return next()
+        
+        const updatedBookReview = await BookReview.findByIdAndUpdate(bookreviewId, req.body,
+        { new:true }
+        )
+
+            return res.redirect(`/bookreviews/${bookreviewId}`);
+        } catch (error) {
+          console.log(error.message)
+          return res.render('bookreviews/edit.ejs', {
+            errorMessage: error.message,
+            bookreview:req.body, 
+    })
+}
+})
 
 // ! Routes that DO NOT render a web page
 // Create - create a new bookreview
